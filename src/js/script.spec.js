af2efae08a2db5bd9b022402825d0c1493802ff9
roge5716 <roger.avalos@rackspace.com>
2017-10-25 15:58:43 -0500

    added mock test

--- src/js/script.spec.js
+++ src/js/script.spec.js
@@ -4,8 +4,17 @@ describe('jquery plugin', () => {
     $.fn.myPattern();
   });
 
-  it('plugin is defined', () => {
+  it('should show plugin as defined', () => {
     expect($.fn.myPattern).to.not.be.undefined;
+    expect($.fn.myPattern.test.someAction).to.not.be.undefined;
+  });
+
+  it('should add a & b', () => {
+    const data = {
+      a: 2,
+      b: 2,
+    };
+    expect($.fn.myPattern.test.someAction(data)).to.eql(4);
   });
 
 });
